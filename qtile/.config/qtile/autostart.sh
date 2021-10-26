0f19602034bb24ba46ca8989045ecb16316de780
Kyle Briggs <kyle.briggs@specopssoft.com>
2021-10-26 19:42:32 -0400

    Adding the qtile changes to test in new branch

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-# systray battery icon
-# systray volume
-volumeicon &
 nm-applet & 
 dropbox start &
 
+numlockx &
+
+#picom --experimental-backends &
+#dunst &
+#unclutter &
+#blueberry-tray &
+#greenclip daemon &
+#light-locker --lock-after-screensaver=0 --no-lock-on-suspend --no-lock-on-lid --no-idle-hint &
+#xidlehook --not-when-fullscreen --not-when-audio --timer 423 'screensaver' '' --timer 180 'xset dpms force off; lockscreen.sh' '' &
+#flashfocus &
+#redshift-gtk -l 44.3894:-79.6903 &
+#~/.config/eww/scripts/getweather &
+#feh-blur --darken 0 -b 6 &
+#eww daemon
+#while true; do
+#    if eww ping; then 
+#        eww open border &
+#        eww open border1 &
+#        eww open border2 &
+#        break
+#    fi
+#done &
--- qtile/.config/qtile/settings/widgets.py
+++ qtile/.config/qtile/settings/widgets.py
@@ -61,6 +61,55 @@ def workspaces():
         separator(),
     ]
 
+# Define functions for bar
+# def taskwarrior():
+# 	return (
+# 		subprocess.check_output(["./.config/qtile/task_polybar.sh"])
+# 		.decode("utf-8")
+# 		.strip()
+# 	)
+# 
+
+### Mouse_callback functions
+# def open_launcher():
+# 	qtile.cmd_spawn("./.config/rofi/launchers/colorful/launcher.sh")
+# 
+# 
+# def finish_task():
+# 	qtile.cmd_spawn('task "$((`cat /tmp/tw_polybar_id`))" done')
+# 
+# 
+# def kill_window():
+# 	qtile.cmd_spawn("xdotool getwindowfocus windowkill")
+# 
+# 
+# def update():
+# 	qtile.cmd_spawn(terminal + "-e yay")
+# 
+# 
+# def open_pavu():
+# 	qtile.cmd_spawn("pavucontrol")
+# 
+# 
+# def toggle_bluetooth():
+# 	qtile.cmd_spawn("./.config/qtile/system-bluetooth-bluetoothctl.sh --toggle")
+# 
+# 
+# def open_bt_menu():
+# 	qtile.cmd_spawn("blueberry")
+# 
+# 
+# def open_connman():
+# 	qtile.cmd_spawn("connman-gtk")
+# 
+# 
+# def todays_date():
+# 	qtile.cmd_spawn("./.config/qtile/calendar.sh")
+# 
+# 
+# def open_powermenu():
+# 	qtile.cmd_spawn("./.config/rofi/powermenu/powermenu.sh")
+
 
 primary_widgets = [
     *workspaces(),
@@ -108,6 +157,312 @@ primary_widgets = [
     widget.Systray(background=colors['dark'], padding=5),
 ]
 
+colors2 = [
+	["#2e3440", "#2e3440"],  # 0 background
+	["#d8dee9", "#d8dee9"],  # 1 foreground
+	["#3b4252", "#3b4252"],  # 2 background lighter
+	["#bf616a", "#bf616a"],  # 3 red
+	["#a3be8c", "#a3be8c"],  # 4 green
+	["#ebcb8b", "#ebcb8b"],  # 5 yellow
+	["#81a1c1", "#81a1c1"],  # 6 blue
+	["#b48ead", "#b48ead"],  # 7 magenta
+	["#88c0d0", "#88c0d0"],  # 8 cyan
+	["#e5e9f0", "#e5e9f0"],  # 9 white
+	["#4c566a", "#4c566a"],  # 10 grey
+	["#d08770", "#d08770"],  # 11 orange
+	["#8fbcbb", "#8fbcbb"],  # 12 super cyan
+	["#5e81ac", "#5e81ac"],  # 13 super blue
+	["#242831", "#242831"],  # 14 super dark background
+]
+
+group_box_settings = {
+	"padding": 5,
+	"borderwidth": 4,
+	"active": colors[9],
+	"inactive": colors[10],
+	"disable_drag": True,
+	"rounded": True,
+	"highlight_color": colors[2],
+	"block_highlight_text_color": colors[6],
+	"highlight_method": "block",
+	"this_current_screen_border": colors[14],
+	"this_screen_border": colors[7],
+	"other_current_screen_border": colors[14],
+	"other_screen_border": colors[14],
+	"foreground": colors[1],
+	"background": colors[14],
+	"urgent_border": colors[3],
+}
+# genome_widgets = [
+# 	widget.TextBox(
+# 		text="",
+# 		foreground=colors2[13],
+# 		background=colors2[0],
+# 		font="Font Awesome 5 Free Solid",
+# 		fontsize=28,
+# 		padding=20,
+# #		#mouse_callbacks={"Button1": open_launcher},
+# 	),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.GroupBox(
+# 				font="Font Awesome 5 Brands",
+# 				visible_groups=[""],
+# 				**group_box_settings,
+# 			),
+# 			widget.GroupBox(
+# 				font="Font Awesome 5 Free Solid",
+# 				visible_groups=["", "", "", "", ""],
+# 				**group_box_settings,
+# 			),
+# 			widget.GroupBox(
+# 				font="Font Awesome 5 Brands",
+# 				visible_groups=[""],
+# 				**group_box_settings,
+# 			),
+# 			widget.GroupBox(
+# 				font="Font Awesome 5 Free Solid",
+# 				visible_groups=["", "", ""],
+# 				**group_box_settings,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				background=colors2[0],
+# 				padding=10,
+# 				size_percent=40,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.CurrentLayoutIcon(
+# 				custom_icon_paths=[os.path.expanduser("~/.config/qtile/icons")],
+# 				foreground=colors2[2],
+# 				background=colors2[14],
+# 				padding=-10,
+# 				scale=0.40,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				padding=10,
+# 				size_percent=50,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.GenPollText(
+# 				func=taskwarrior,
+# 				update_interval=5,
+# 				foreground=colors2[11],
+# 				background=colors2[14],
+# 				#mouse_callbacks={"Button1": finish_task},
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Spacer(),
+# 			widget.TextBox(
+# 			    text=" ",
+# 			    foreground=colors2[12],
+# 			    background=colors2[0],
+# 			    # fontsize=38,
+# 			    font="Font Awesome 5 Free Solid",
+# 			),
+# 			widget.WindowName(
+# 			    background=colors2[0],
+# 			    foreground=colors2[12],
+# 			    width=bar.CALCULATED,
+# 			    empty_group_string="Desktop",
+# 			    max_chars=130,
+# 			    #mouse_callbacks={"Button2": kill_window},
+# 			),
+# 			widget.Spacer(),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Systray(icon_size=26, background=colors2[14], padding=10),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				padding=10,
+# 				size_percent=50,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.TextBox(
+# 				text=" ",
+# 				foreground=colors2[8],
+# 				background=colors2[14],
+# 				font="Font Awesome 5 Free Solid",
+# 				# fontsize=38,
+# 			),
+# 			widget.PulseVolume(
+# 				foreground=colors2[8],
+# 				background=colors2[14],
+# 				limit_max_volume="True",
+# 				#mouse_callbacks={"Button3": open_pavu},
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				padding=10,
+# 				size_percent=50,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.TextBox(
+# 				text=" ",
+# 				font="Font Awesome 5 Free Solid",
+# 				foreground=colors2[7],  # fontsize=38
+# 				background=colors2[14],
+# 			),
+# 			widget.Wlan(
+# 				interface="wlp58s0",
+# 				format="{essid}",
+# 				foreground=colors2[7],
+# 				background=colors2[14],
+# 				padding=5,
+# 				mouse_callbacks={"Button1": open_connman},
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				padding=10,
+# 				size_percent=50,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.TextBox(
+# 				text=" ",
+# 				font="Font Awesome 5 Free Solid",
+# 				foreground=colors2[5],  # fontsize=38
+# 				background=colors2[14],
+# 			),
+# 			widget.Clock(
+# 				format="%a, %b %d",
+# 				background=colors2[14],
+# 				foreground=colors2[5],
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.Sep(
+# 				linewidth=0,
+# 				foreground=colors2[2],
+# 				padding=10,
+# 				size_percent=50,
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.TextBox(
+# 				text=" ",
+# 				font="Font Awesome 5 Free Solid",
+# 				foreground=colors2[4],  # fontsize=38
+# 				background=colors2[14],
+# 			),
+# 			widget.Clock(
+# 				format="%I:%M %p",
+# 				foreground=colors2[4],
+# 				background=colors2[14],
+# 				#    mouse_callbacks={"Button1": todays_date},
+# 			),
+# 			widget.TextBox(
+# 				text="",
+# 				foreground=colors2[14],
+# 				background=colors2[0],
+# 				fontsize=28,
+# 				padding=0,
+# 			),
+# 			widget.TextBox(
+# 				text="⏻",
+# 				foreground=colors2[13],
+# 				font="Font Awesome 5 Free Solid",
+# 				fontsize=34,
+# 				padding=20,
+# 				#mouse_callbacks={"Button1": open_powermenu},
+# 			),
+# 	]
 secondary_widgets = [
     *workspaces(),
 
